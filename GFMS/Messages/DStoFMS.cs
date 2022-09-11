0e2ede4fcfc6411a4ab126145c6fbdde61f087e9
Gregory Kelly <git@gregk.ca>
2022-09-11 16:37:20 -0400

    Create class to represent messasges from DS to FMS, requisite additions to Message base class

---
+++ GFMS/Messages/DStoFMS.cs
@@ -0,0 +1,97 @@
+﻿namespace GFMS.Messages
+{
+    public class DStoFMS : Message
+    {
+        public ushort SequenceNum;
+
+        private byte _statusByte;
+        public bool EStopped
+        {
+            get => CheckBit(_statusByte, 7);
+            set => SetBit(ref _statusByte, 7, value);
+        }
+        public bool CommsActive
+        {
+            get => CheckBit(_statusByte, 5);
+            set => SetBit(ref _statusByte, 5, value);
+        }
+        public bool RadioConnected
+        {
+            get => CheckBit(_statusByte, 4);
+            set => SetBit(ref _statusByte, 4, value);
+        }
+        public bool RioConnected
+        {
+            get => CheckBit(_statusByte, 3);
+            set => SetBit(ref _statusByte, 3, value);
+        }
+        public bool Enabled
+        {
+            get => CheckBit(_statusByte, 2);
+            set => SetBit(ref _statusByte, 2, value);
+        }
+        public Mode Mode
+        {
+            get => (Mode)(_statusByte & 0b00000011);
+            set
+            {
+                // Clear bits for value
+                _statusByte &= 0b11111100;
+                // Or in value
+                _statusByte |= (byte)value;
+            }
+        }
+
+        /// <summary>
+        /// Raw byte representation of battery voltage
+        /// Per docs actual voltage is calculated from byte XXYY as XX + YY/256
+        /// </summary>
+        private ushort _voltageRaw;
+        public ushort BatteryVoltage
+        {
+            get => (ushort)((_voltageRaw >> 8) + (_voltageRaw & 0b1111) / 256.0);
+            set => _voltageRaw = (ushort)(((byte)(value / 256) << 8) | (byte)(value % 256));
+        }
+
+        public ushort TeamNum;
+
+        public override (byte[], int) ToByteArray()
+        {
+            byte[] data = new byte[64];
+            int idx = 0;
+
+            // Sequence Num
+            WriteNumber(SequenceNum, ref data, ref idx);
+            // Comm Version
+            data[idx++] = 0;
+            // Control byte
+            data[idx++] = _statusByte;
+
+            // Team Number
+            WriteNumber(TeamNum, ref data, ref idx);
+
+            // Battery
+            WriteNumber(BatteryVoltage, ref data, ref idx);
+
+            return (data, idx);
+        }
+
+        protected override void FromByteArray(byte[] data)
+        {
+            int idx = 0;
+
+            // Sequence Num
+            SequenceNum = ReadShort(data, ref idx);
+            // Comm Version
+            _ = data[idx++];
+            // Control byte
+            _statusByte = data[idx++];
+
+            // Team Number
+            TeamNum = ReadShort(data, ref idx);
+
+            // Battery
+            _voltageRaw = ReadShort(data, ref idx);
+        }
+    }
+}
--- GFMS/Messages/Message.cs
+++ GFMS/Messages/Message.cs
@@ -8,7 +8,10 @@ namespace GFMS.Messages
 {
     public abstract class Message
     {
+        public Message() { }
+
         public abstract (byte[], int) ToByteArray();
+        protected abstract void FromByteArray(byte[] data);
 
         protected static void WriteNumber(ushort num, ref byte[] arr, ref int idx)
         {
@@ -37,6 +40,46 @@ namespace GFMS.Messages
             arr[idx++] = bytes[5];
             arr[idx++] = bytes[6];
             arr[idx++] = bytes[7];
+        }
+
+        protected static ushort ReadShort(byte[] arr, ref int idx)
+        {
+            return (ushort)(arr[idx++] << 8 + arr[idx]);
+        }
+        protected static uint ReadInt(byte[] arr, ref int idx)
+        {
+            return (ushort)(arr[idx++] << 16 + arr[idx++] << 8 + arr[idx]);
+        }
+        protected static ulong ReadLong(byte[] arr, ref int idx)
+        {
+            return (ushort)(arr[idx++] << 24 + arr[idx++] << 16 + arr[idx++] << 8 + arr[idx]);
+        }
+
+        protected static bool CheckBit(byte subject, int bit)
+        {
+            return (subject & 0b1 << bit) != 0;
+        }
+
+        protected static void SetBit(ref byte subject, int bit, bool val)
+        {
+            if (val)
+                subject |= (byte)(0b1 << bit);
+            else
+                subject &= (byte)(0b0 << bit);
+        }
+
+        /// <summary>
+        /// Create a new message of specified type from byte array
+        /// </summary>
+        /// <typeparam name="T">Type of message to create</typeparam>
+        /// <param name="bytes">Byte array containing data</param>
+        /// <returns>Created message</returns>
+        public static T FromBytes<T>(byte[] bytes)
+            where T : Message, new()
+        {
+            T message = new();
+            message.FromByteArray(bytes);
+            return message;
         }
     }
 
