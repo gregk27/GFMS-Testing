5dc867b613d571d1bc78074bf6c7282e97ea6fb5
Gregory Kelly <git@gregk.ca>
2022-09-11 17:31:02 -0400

    Fix endianess and word length bugs in Message utility functions

--- GFMS/Messages/Message.cs
+++ GFMS/Messages/Message.cs
@@ -16,41 +16,51 @@ namespace GFMS.Messages
         protected static void WriteShort(ushort num, ref byte[] arr, ref int idx)
         {
             var bytes = BitConverter.GetBytes(num);
-            arr[idx++] = bytes[0];
             arr[idx++] = bytes[1];
+            arr[idx++] = bytes[0];
         }
         protected static void WriteInt(uint num, ref byte[] arr, ref int idx)
         {
             var bytes = BitConverter.GetBytes(num);
-            arr[idx++] = bytes[0];
-            arr[idx++] = bytes[1];
-            arr[idx++] = bytes[2];
             arr[idx++] = bytes[3];
+            arr[idx++] = bytes[2];
+            arr[idx++] = bytes[1];
+            arr[idx++] = bytes[0];
         }
         protected static void WriteLong(ulong num, ref byte[] arr, ref int idx)
         {
             var bytes = BitConverter.GetBytes(num);
-            arr[idx++] = bytes[0];
-            arr[idx++] = bytes[1];
-            arr[idx++] = bytes[2];
-            arr[idx++] = bytes[3];
-            arr[idx++] = bytes[4];
-            arr[idx++] = bytes[5];
-            arr[idx++] = bytes[6];
             arr[idx++] = bytes[7];
+            arr[idx++] = bytes[6];
+            arr[idx++] = bytes[5];
+            arr[idx++] = bytes[4];
+            arr[idx++] = bytes[3];
+            arr[idx++] = bytes[2];
+            arr[idx++] = bytes[1];
+            arr[idx++] = bytes[0];
         }
 
         protected static ushort ReadShort(byte[] arr, ref int idx)
         {
-            return (ushort)(arr[idx++] << 8 + arr[idx]);
+            return (ushort)((arr[idx++] << 8) + arr[idx++]);
         }
         protected static uint ReadInt(byte[] arr, ref int idx)
         {
-            return (ushort)(arr[idx++] << 16 + arr[idx++] << 8 + arr[idx]);
+            uint val = 0;
+            for (int i = 3; i >= 0; i--)
+            {
+                val += (uint)(arr[idx++] << i * 8);
+            }
+            return val;
         }
         protected static ulong ReadLong(byte[] arr, ref int idx)
         {
-            return (ushort)(arr[idx++] << 24 + arr[idx++] << 16 + arr[idx++] << 8 + arr[idx]);
+            ulong val = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                val += (ulong)(arr[idx++] << i * 8);
+            }
+            return val;
         }
 
         protected static bool CheckBit(byte subject, int bit)
