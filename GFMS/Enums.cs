412c3dc1785f2cf0dd6b056c60c2044d77194516
Gregory Kelly <git@gregk.ca>
2022-09-11 22:40:47 -0400

    Move enums out of Message into own class

---
+++ GFMS/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace GFMS
+{
+    public enum Mode
+    {
+        TELE = 0,
+        TEST = 1,
+        AUTO = 2,
+    }
+
+    public enum Alliance
+    {
+        RED = 0,
+        BLUE = 1,
+    }
+
+    public enum TournamentLevel
+    {
+        TEST = 0,
+        PRACTICE = 1,
+        QUALS = 2,
+        ELIMS = 3,
+    }
+}
