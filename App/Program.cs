0575c7c8f5fb1c090589b0c9911cb048befa181d
Gregory Kelly <git@gregk.ca>
2022-09-23 16:15:53 -0400

    Add sketchy console support for now

--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace App
 {
     internal static class Program
@@ -11,7 +13,19 @@ namespace App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            // Spawn console to get log info until a better solution is added
+            AllocConsole();
+            Console.WriteLine("Hello World!");
             Application.Run(new Form1());
+            FreeConsole();
         }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FreeConsole();
     }
 }
